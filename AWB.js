e9ba3ef7b9de2ad00016f2e4e11c9e53c93d9904
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-09-15 18:43:43 +0100

    Update AWB.js

--- AWB.js
+++ AWB.js
@@ -1,4 +1,9 @@
 //Enables Autowoot
 API.on(API.DJ_ADVANCE,autoWoot);function autoWoot(){$('.button')[2].click();}$('.button')[2].click()
-//Sends Chat Message
+//Sends Chat Message about autowoot
 API.chatLog('Thank you for using SuperW00T Version 1.0.0!')
+//Sends Chat Message about updates coming soon
+API.chatLog('More Updates coming soon!')
+//AutoQueue documentation
+javascript:API.on(API.WAIT_LIST_UPDATE, callback);function callback(){var waitingCount = API.getWaitList().length;if (waitingCount<50){API.djJoin();}}
+
