e70e976b6639b8946c151b653f7495840838939b
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-09-15 19:04:34 +0100

    Update AWB.js

--- AWB.js
+++ AWB.js
@@ -6,4 +6,5 @@ API.chatLog('Thank you for using SuperW00T Version 1.0.0!')
 API.chatLog('Type /aj to begin AutoJoining the waitlist!')
 //AutoQueue documentation by Shard
 function joinCommandCalled(e){if(e=="/aj"){API.chatLog("AutoJoin confirmed");var t=API.getTimeRemaining();setTimeout(function(){var e=setInterval(function(){API.djJoin();if(API.getWaitListPosition(user)>-1||API.getTimeRemaining()>1){user=null;clearInterval(e);console.log("join failed or successful")}},40)},t*1e3-800);API.on(API.WAIT_LIST_UPDATE,n);function n(){var e=API.getWaitList().length;if(e<50){API.djJoin()}}}}API.on(API.CHAT_COMMAND,joinCommandCalled)
-
+//Strobe Script
+if ("/strobe on" == a) {log(Models.user.data.username + ' hit the strobe light!'); return RoomUser.audience.strobeMode(true), !0;};
