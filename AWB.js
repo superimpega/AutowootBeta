3ac0b7291c292ce41c198cfb6801d85f27964dbc
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-09-15 18:51:48 +0100

    Update AWB.js

--- AWB.js
+++ AWB.js
@@ -5,5 +5,5 @@ API.chatLog('Thank you for using SuperW00T Version 1.0.0! /aj to start autojoin'
 //Sends Chat Message about updates coming soon
 API.chatLog('More Updates coming soon!')
 //AutoQueue documentation by Shard
-function joinCommandCalled(e){if(e=="/aj"){console.log("command recognised");var t=API.getTimeRemaining();setTimeout(function(){var e=setInterval(function(){API.djJoin();if(API.getWaitListPosition(user)>-1||API.getTimeRemaining()>1){user=null;clearInterval(e);console.log("join failed or successful")}},40)},t*1e3-800);API.on(API.WAIT_LIST_UPDATE,n);function n(){var e=API.getWaitList().length;if(e<50){API.djJoin()}}}}API.on(API.CHAT_COMMAND,joinCommandCalled)
+function joinCommandCalled(e){if(e=="/aj"){API.chatLog("AutoJoin confirmed");var t=API.getTimeRemaining();setTimeout(function(){var e=setInterval(function(){API.djJoin();if(API.getWaitListPosition(user)>-1||API.getTimeRemaining()>1){user=null;clearInterval(e);console.log("join failed or successful")}},40)},t*1e3-800);API.on(API.WAIT_LIST_UPDATE,n);function n(){var e=API.getWaitList().length;if(e<50){API.djJoin()}}}}API.on(API.CHAT_COMMAND,joinCommandCalled)
 
