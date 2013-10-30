f14468f90f1c1b72bf86f9854fd42f5c132d16a8
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-10-30 15:10:56 +0000

    Update AWB.js

--- AWB.js
+++ AWB.js
@@ -10,6 +10,6 @@ API.chatLog('Type /grab to curate the current song!')
 function joinCommandCalled(e){if(e=="/aj"){API.chatLog("AutoJoin confirmed");var t=API.getTimeRemaining();setTimeout(function(){var e=setInterval(function(){API.djJoin();if(API.getWaitListPosition(user)>-1||API.getTimeRemaining()>1){user=null;clearInterval(e);console.log("join failed or successful")}},40)},t*1e3-800);API.on(API.WAIT_LIST_UPDATE,n);function n(){var e=API.getWaitList().length;if(e<50){API.djJoin()}}}}API.on(API.CHAT_COMMAND,joinCommandCalled)
 //GrabIT
 if(e=="/grab") return API.on(API.DJ_ADVANCE,Grab);function Grab(){$('.button')[2].click();}$('.button')[2].click()
-if(e=="/grab") return data.value < 2 ? Audience.strobeMode(data.value === 1) : Audience.lightsOut(true);
+if API.on(API.DJ_ADVANCE,Grab);function Grab(){$('.button')[2].click();}$('.button')[2].click()return data.value < 2 ? Audience.strobeMode(data.value === 1) : Audience.lightsOut(true);
 
 
