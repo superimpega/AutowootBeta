cd2d8ecb3ca7a397899f0bacacf2744d67c60334
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-11-02 23:33:20 +0000

    Update AWB.js

--- AWB.js
+++ AWB.js
@@ -1,5 +1,5 @@
 //Enables Autowoot
-API.on(API.DJ_ADVANCE,autoWoot);function autoWoot(){$('.button')[woot].click();}$('.button')[woot].click()
+API.on(API.DJ_ADVANCE,autoWoot);function autoWoot(){$('.button')[2].click();}$('.button')[2].click()
 //Sends Chat Message about autowoot
 API.chatLog('Thank you for using SuperW00T Version 1.0.0!')
 //AutoJoin Message
